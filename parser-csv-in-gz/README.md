5b8ca70b9c965cca30e8adb51937fe469178d952
aofdev <aof.dev@gmail.com>
2021-06-18 17:27:20 +0700

    [+] Add single-perf-csv

--- parser-csv-in-gz/README.md
+++ parser-csv-in-gz/README.md
@@ -4,12 +4,18 @@
 ```bash
 # Run with Cargo
 cargo run -- datasets/2019-Oct.test.csv.gz
+
+# If Run single-perf-csv
+cargo run -- < datasets/2019-Oct.test.csv
 ```
 
 ## Build 🚀
 ```bash
 # build a single
 cargo build --release --bin single
+
+# build a single-perf-csv
+cargo build --release --bin single-perf-csv
 
 # build a parallel
 cargo build --release --bin parallel
---
+++ parser-csv-in-gz/src/single-perf-csv.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use std::io;
+
+use regex::bytes::RegexBuilder;
+use csv::{ByteRecord, ReaderBuilder};
+
+struct Stats {
+    total: usize,
+    matching: usize,
+}
+
+fn main() {
+    let result: Stats = count().expect(&format!("Couldn't read"));
+    let percent = 100.0 * result.matching as f32 / result.total as f32;
+    println!("match: {} / {} Total = {:.2}%", result.matching, result.total, percent);
+}
+
+fn count() -> Result<Stats, Box<dyn Error>> {
+    let mut reader = ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(io::stdin());
+
+    let regex = RegexBuilder::new(r"(205).*").unicode(false).build().unwrap();
+    let mut record = ByteRecord::new();
+    let mut total = 0;
+    let mut matching = 0;
+ 
+    while let Ok(true) = reader.read_byte_record(&mut record) {
+        total += 1;
+        if regex.is_match(&record[3]) {
+            matching += 1;
+        }
+    }
+
+    Ok(Stats { total, matching })
+
+}
