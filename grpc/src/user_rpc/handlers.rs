213b5245754e27301bf92605a16ea04bc9fb8e44
aofdev <aof.dev@gmail.com>
2021-09-10 22:23:55 +0700

    [.] Update grpc crud

--- grpc/src/user_rpc/handlers.rs
+++ grpc/src/user_rpc/handlers.rs
@@ -1,32 +1,89 @@
 use crate::diesel::QueryDsl;
 use crate::diesel::RunQueryDsl;
 use crate::schema::users::dsl::*;
-use crate::user_rpc::models::User;
+use crate::user_rpc::models::{InputUser, NewUser, User};
 use crate::Pool;
-use diesel::dsl::{delete, insert_into};
+use diesel::dsl::{delete, insert_into, update};
 use std::vec::Vec;
 
 pub async fn get_users(db: Pool) -> Vec<User> {
-    match db_get_all_users(db).await {
+    match get_all_users_db(db).await {
         Ok(items) => items,
         Err(e) => panic!("Error get_users: {}", e),
     }
 }
 
-pub async fn get_user_by_id(db: Pool, user_id: i32) -> User {
-    match db_get_user_by_id(db, user_id) {
+pub async fn get_user(db: Pool, user_id: i32) -> User {
+    match get_user_db(db, user_id).await {
         Ok(item) => item,
-        Err(e) => panic!("Error get_user_by_id: {}", e),
+        Err(e) => panic!("Error get_user: {}", e),
     }
 }
 
-async fn db_get_all_users(pool: Pool) -> Result<Vec<User>, diesel::result::Error> {
+pub async fn add_user(db: Pool, input_user: InputUser) -> User {
+    match add_user_db(db, input_user).await {
+        Ok(item) => item,
+        Err(e) => panic!("Error add_user: {}", e),
+    }
+}
+
+pub async fn update_user(db: Pool, user_id: i32, input_user: InputUser) -> User {
+    match update_user_db(db, user_id, input_user).await {
+        Ok(item) => item,
+        Err(e) => panic!("Error update_user: {}", e),
+    }
+}
+
+pub async fn delete_user(db: Pool, user_id: i32) -> usize {
+    match delete_user_db(db, user_id).await {
+        Ok(item) => item,
+        Err(e) => panic!("Error delete_user: {}", e),
+    }
+}
+
+async fn get_all_users_db(pool: Pool) -> Result<Vec<User>, diesel::result::Error> {
     let conn = pool.get().unwrap();
     let items = users.load::<User>(&conn)?;
     Ok(items)
 }
 
-fn db_get_user_by_id(pool: Pool, user_id: i32) -> Result<User, diesel::result::Error> {
+async fn get_user_db(pool: Pool, user_id: i32) -> Result<User, diesel::result::Error> {
     let conn = pool.get().unwrap();
     users.find(user_id).get_result::<User>(&conn)
+}
+
+async fn add_user_db(pool: Pool, item: InputUser) -> Result<User, diesel::result::Error> {
+    let conn = pool.get().unwrap();
+    let new_user = NewUser {
+        first_name: &item.first_name,
+        last_name: &item.last_name,
+        email: &item.email,
+        created_at: chrono::Local::now().naive_local(),
+    };
+    let res = insert_into(users).values(&new_user).get_result(&conn)?;
+    Ok(res)
+}
+
+async fn update_user_db(
+    pool: Pool,
+    user_id: i32,
+    item: InputUser,
+) -> Result<User, diesel::result::Error> {
+    let conn = pool.get().unwrap();
+    let updated = update(users.find(user_id))
+        .set(User {
+            id: user_id,
+            first_name: item.first_name.to_string(),
+            last_name: item.last_name.to_string(),
+            email: item.email.to_string(),
+            created_at: chrono::Local::now().naive_local(),
+        })
+        .get_result(&conn)?;
+    Ok(updated)
+}
+
+async fn delete_user_db(pool: Pool, user_id: i32) -> Result<usize, diesel::result::Error> {
+    let conn = pool.get().unwrap();
+    let count = delete(users.find(user_id)).execute(&conn)?;
+    Ok(count)
 }
--- grpc/src/user_rpc/models.rs
+++ grpc/src/user_rpc/models.rs
@@ -1,14 +1,14 @@
 use crate::schema::*;
 use serde::{Deserialize, Serialize};
 
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct InputUser {
-//     pub first_name: String,
-//     pub last_name: String,
-//     pub email: String,
-// }
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InputUser {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+}
 
-#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[derive(Debug, AsChangeset, Serialize, Deserialize, Queryable)]
 pub struct User {
     pub id: i32,
     pub first_name: String,
@@ -17,11 +17,11 @@ pub struct User {
     pub created_at: chrono::NaiveDateTime,
 }
 
-// #[derive(Insertable, Debug)]
-// #[table_name = "users"]
-// pub struct NewUser<'a> {
-//     pub first_name: &'a str,
-//     pub last_name: &'a str,
-//     pub email: &'a str,
-//     pub created_at: chrono::NaiveDateTime,
-// }
+#[derive(Insertable, Debug)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub first_name: &'a str,
+    pub last_name: &'a str,
+    pub email: &'a str,
+    pub created_at: chrono::NaiveDateTime,
+}
