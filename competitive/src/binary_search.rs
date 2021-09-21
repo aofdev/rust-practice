fee5f2dbf5a84002877f1f166d3d44f6bd9acc4c
aofdev <aof.dev@gmail.com>
2021-09-21 21:56:38 +0700

    [.] Update practice competitive

--- competitive/src/binary_search.rs
+++ competitive/src/binary_search.rs
@@ -7,8 +7,7 @@ pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
     let (mut low, mut high) = (0, nums.len());
 
     while low < high {
-        dbg!(low, high);
-        let mid = dbg!(low + (high - low) / 2);
+        let mid = low + (high - low) / 2;
         match target.cmp(&nums[mid]) {
             Ordering::Equal => return mid as i32,
             Ordering::Less => high = mid,
@@ -16,6 +15,29 @@ pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
         }
     }
     -1
+}
+
+// https://leetcode.com/problems/first-bad-version/
+fn is_bad_version(version: i32) -> bool {
+    // bad version 5-8
+    match version {
+        5..=8 => true,
+        _ => false,
+    }
+}
+pub fn first_bad_version(n: i32) -> i32 {
+    let mut left = 1;
+    let mut right = n;
+
+    while left != right {
+        let middle = left + (right - left) / 2;
+        if is_bad_version(middle) {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+    }
+    left
 }
 
 pub fn binary_search_2(k: i32, items: &[i32]) -> Option<usize> {
@@ -51,6 +73,14 @@ fn test_binary_search() {
     let nums = vec![-1, 0, 3, 5, 9, 12];
     assert_eq!(4, binary_search(nums.clone(), 9));
     assert_eq!(-1, binary_search(nums.clone(), 2));
+}
+
+#[test]
+fn test_first_bad_version() {
+    assert_eq!(5, first_bad_version(6));
+    assert_eq!(5, first_bad_version(8));
+    assert_eq!(2, first_bad_version(2));
+    assert_eq!(4, first_bad_version(4));
 }
 
 #[test]
